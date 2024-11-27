fe67f476ac19dbdf36793f09857d8664ec1203a2
unknown <selinaadwoa1@gmail.com>
2024-11-27 08:39:49 -0800

    added files in my index.html and index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+let friend = prompt("what is your name");
+let age = prompt ("what is your age");
+let email = prompt ("what is your email address");
+
+alert (`Hi your name is ${friend}, you are ${age} years old and we will keep in touch via your email: ${email}   🤙🤙`);
+
